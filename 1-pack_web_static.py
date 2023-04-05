e26087bffbce7c72016fd6777c2cfb774a0b801f
Lawrence Nno <Lawrence.Nno@gmail.com>
2023-04-05 20:40:38 +0100

    TAsk 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,16 +7,16 @@ from fabric.api import local
 
 def do_pack():
     """Create a tar gzipped archive of the directory web_static."""
-    time = datetime.utcnow()
-    content = "versions/web_static_{}{}{}{}{}{}.tgz".format(time.year,
-                                                         time.month,
-                                                         time.day,
-                                                         time.hour,
-                                                         time.minute,
-                                                         time.second)
+    t = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(t.year,
+                                                         t.month,
+                                                         t.day,
+                                                         t.hour,
+                                                         t.minute,
+                                                         t.second)
     if os.path.isdir("versions") is False:
         if local("mkdir -p versions").failed is True:
             return None
-    if local("tar -cvzf {} web_static".format(content)).failed is True:
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
-    return content
+    return file
